ee07de08061391ef42454a67f245bb6cd37bce97
Vengatesh M <vengateshm.13@gmail.com>
2022-01-14 21:07:21 +0530

    splash screen created

---
+++ app/src/main/java/dev/vengateshm/eventhub/presentation/SplashScreen.kt
@@ -0,0 +1,36 @@
+package dev.vengateshm.eventhub.presentation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import dev.vengateshm.eventhub.R
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = Color.White
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_app_logo),
+            contentDescription = "App icon"
+        )
+    }
+}
+
+@Preview
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen()
+}
